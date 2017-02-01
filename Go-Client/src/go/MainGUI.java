df9e477b03d3103d56a23ffd3a4bbfe26a0a1e79
Maro Astralo <Maro Astralo@192.168.0.115>
2017-02-01 20:04:38 +0100

    Deleted unused classes

--- Go-Client/src/go/MainGUI.java
+++ Go-Client/src/go/MainGUI.java
@@ -11,7 +11,6 @@ import java.awt.event.WindowEvent;
 import java.io.IOException;
 
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
